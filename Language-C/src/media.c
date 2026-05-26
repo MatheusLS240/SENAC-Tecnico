f46b9adffd2b6aef436de9e24a7fcc4175bb4e36
Matheus <grandboss1978@gmail.com>
2026-05-26 11:55:26 -0300

    Editado o codigo da media

--- Language-C/src/media.c
+++ Language-C/src/media.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     float num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+
+    system("clear");
 
     printf("Insira a primeira nota");
     scanf("%f", &num1);
@@ -14,9 +17,21 @@ int main() {
 
     float media = (num1 + num2 + num3 + num4) / 4;
 
+    system("clear");
+
     if (media >= 7) {
-        printf("Aprovado!");
+        printf("\nAprovado!\n");
     } else {
-        printf("Reprovado!");
+        printf("\nReprovado!\n");
     }
+
+    printf("\n##################################\n");
+    printf("Endereço da memória da variavel num1: %p \n", &num1);
+    printf("Endereço da memória da variavel num2: %p \n", &num2);
+    printf("Endereço da memória da variavel num3: %p \n", &num3);
+    printf("Endereço da memória da variavel num4: %p \n", &num4);
+    printf("Endereço da memória da variavel media: %p \n", &media);
+
+
+    return 0;
 }
