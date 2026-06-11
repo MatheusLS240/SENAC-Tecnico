1dffb1eea16b10c81fb678b592bcf558f5a15f26
Matheus <grandboss1978@gmail.com>
2026-06-11 10:08:36 -0300

    Adicao de exercicios com while e funcao, criação do ambiente virtual do py

--- Language-Py/for2.py
+++
@@ -1,7 +0,0 @@
-contador = 0
-for ano in range(1950, 2027):
-    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-        print(f"{ano} é um ano bissexto.")
-        contador += 1
-
-print(f"Total de anos bissextos entre 1950 e 2026: {contador}")
--- Language-Py/nome.py
+++
@@ -1,3 +0,0 @@
-nome = input("Digite seu nome: ")
-print("Olá, " + nome + "! Bem-vindo ao Python!")
-print(f"Ola, {nome}! Bem-vindo ao Python!")
